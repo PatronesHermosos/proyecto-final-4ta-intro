2950d6c0776e9210de7d115fe5a3a4f1bfa37f86
josyea7 <100484442+josyea7@users.noreply.github.com>
2022-03-23 21:52:44 -0500

    Add files via upload

---
+++ equipo-19/pollution/js/main.js
@@ -0,0 +1,17 @@
+//Swiper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+//Navigation bar effects on scroll
+
+//Responsive navigation menu toggle
