3883410097be5e4a1d02506c8c54536a6075ed1c
margaritaquispe <100645756+margaritaquispe@users.noreply.github.com>
2022-05-05 19:37:51 -0500

    Update patrones-hermosos.js

--- equipo-10/patrones-hermosos.js
+++ equipo-10/patrones-hermosos.js
@@ -1,20 +1,18 @@
-
-
 function Mostrar(){
     document.getElementById("img").style.display = "block";
 }
 function Ocultar(){
-    Document.getElementById("img").style.display = "none";
+    document.getElementById("img").style.display = "none";
 }
 function Mostrar_Ocultar(){
-    document.getElementById("img").style.display:"block";
+    // document.getElementById("img").style.display = "block";
     var img = document.getElementById("img");
-if(img.style.display == "none"){
+    if(img.style.display == "none"){
     Mostrar();
-    document.getElementById("boton").value ="Mostrar";
-}
-else{
+    // document.getElementById("boton").value ="Mostrar";
+    }
+    else{
     Ocultar();
-    document.getElementById("boton").value ="Ocultar";
-  }
+    // document.getElementById("boton").value ="Ocultar";
+    }
 }
